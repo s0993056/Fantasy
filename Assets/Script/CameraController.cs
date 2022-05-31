4d7a611da2a25d4cce6501ae6c35ee2d69cd788a
s0993056 <s0993056@alumni.ncyu.edu.tw>
2022-05-31 16:04:44 +0800

    5/31

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
     int zoom = 10;
     int y = 4;
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //text.gameObject.GetComponent<Text>;
     }
 
     // Update is called once per frame
@@ -18,16 +20,17 @@ public class CameraController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if(zoom<50) zoom += 5;
-            if (y < 34) y += 5;
+            if (y < 39) y += 5;
             Camera.main.orthographicSize = (float)zoom / 2;
             transform.position = new Vector3(4, (float)y /2,-1);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (y >4&&zoom<41) y -= 5;
+            if (y >4&&zoom<46) y -= 5;
             if (zoom > 10) zoom -= 5;
             Camera.main.orthographicSize = (float)zoom / 2;
             transform.position = new Vector3(4, (float)y /2, -1);
         }
+        text.text = $"√Ë¿Y§j§p : {zoom/5}";
     }
 }
