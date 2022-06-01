475597893944c19d5b1fbc7432a81f082d63308a
s0993056 <105775351+s0993056@users.noreply.github.com>
2022-06-02 05:39:43 +0800

    6/1+

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,9 +5,9 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     Animator animator;
-    public float jumpForce = 200f;
-    public float walkForce = 10f;
-    public float maxSpeed = 2f;
+    public float jumpForce = 1200f;
+    public float walkForce = 20f;
+    public float maxSpeed = 6f;
     static string trigger = "Idle Trigger";
     void Start()
     {
@@ -29,41 +29,43 @@ public class PlayerController : MonoBehaviour
             this.transform.position.y, this.transform.position.z);*/
         float speed = Mathf.Abs(rigid2D.velocity.x);
         int key = 0;
-        /*if (Input.GetKeyDown(KeyCode.Space)&& rigid2D.velocity.y==0)
+        if (Input.GetKeyDown(KeyCode.UpArrow)&& rigid2D.velocity.y< Mathf.Abs(.0001f))
         {
-            animator.SetTrigger("Jump Trigger"); 
+            //TriggerChange("Jump Trigger"); 
             rigid2D.AddForce(transform.up * jumpForce);
-        }*/
-
-        //animator.SetTrigger("Idle Trigger");
-
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             TriggerChange("Walk Trigger");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
             key = -1;
-            rigid2D.AddForce(key * transform.right * walkForce);
+            //rigid2D.velocity = Vector2.left * 6;
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
             TriggerChange("Idle Trigger");
+            //rigid2D.velocity = Vector2.left*2;
+        }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             TriggerChange("Walk Trigger");
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
             key = 1;
-            rigid2D.AddForce(key * transform.right * walkForce);
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
-            TriggerChange("Idle Trigger");
-
-        //if (speed < maxSpeed)
+        {    TriggerChange("Idle Trigger");
+            //rigid2D.AddForce(Vector2.left * walkForce*10);
+        }
+        if (speed < maxSpeed)
+            rigid2D.AddForce(transform.right * walkForce*key);
 
         if (key!=0)
             transform.localScale = new Vector3(key,1,1);
-        /*if (rigid2D.velocity.y == 0)
-            animator.speed = speed / 2f;
-        else animator.speed =0.55f;*/
-
-        //if (transform.position.y < -10)SceneManager.LoadScene("GameScene");
     }
 }
 enum InputKey
