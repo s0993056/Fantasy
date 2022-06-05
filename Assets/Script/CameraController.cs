3c4665e9d53c0796c15e4762e017792cba6fd1b9
s0993056 <105775351+s0993056@users.noreply.github.com>
2022-06-06 01:02:42 +0800

    6/6

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -5,32 +5,18 @@ using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-    int zoom = 10;
-    int y = 4;
-    public Text text;
+    GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
-        //text.gameObject.GetComponent<Text>;
+        Player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if(zoom<50) zoom += 5;
-            if (y < 39) y += 5;
-            Camera.main.orthographicSize = (float)zoom / 2;
-            transform.position = new Vector3(4, (float)y /2,-1);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if (y >4&&zoom<46) y -= 5;
-            if (zoom > 10) zoom -= 5;
-            Camera.main.orthographicSize = (float)zoom / 2;
-            transform.position = new Vector3(4, (float)y /2, -1);
-        }
-        text.text = $"√Ë¿Y§j§p : {zoom/5}";
+        Vector3 playerPos = Player.transform.position;
+        transform.position = new Vector3(
+            playerPos.x,9.5f, -1);
     }
 }
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigid2D;
     Animator animator;
     public float jumpForce = 1200f;
-    public float walkForce = 20f;
+    public float walkForce = 50f;
     public float maxSpeed = 6f;
     static string trigger = "Idle";
     void Start()
@@ -22,7 +22,6 @@ public class PlayerController : MonoBehaviour
             animator.ResetTrigger(trigger);
             trigger = t.ToString();
             animator.SetTrigger(trigger);
-            Debug.Log($"{trigger}");
         }
     }
 
@@ -34,7 +33,7 @@ public class PlayerController : MonoBehaviour
             this.transform.position.y, this.transform.position.z);*/
         float speed = Mathf.Abs(rigid2D.velocity.x);
         int key = 0;
-        if (Input.GetKeyDown(KeyCode.UpArrow)&&  Mathf.Abs(rigid2D.velocity.y)<1e-5f)
+        if (Input.GetKeyDown(KeyCode.UpArrow)&&  Mathf.Abs(rigid2D.velocity.y)<1e-4f)
         {
             TriggerChange(Trigger.Jump); 
             rigid2D.AddForce(transform.up * jumpForce);
@@ -44,39 +43,37 @@ public class PlayerController : MonoBehaviour
         if(rigid2D.velocity.y<-0.1) 
             TriggerChange(Trigger.Fall);
 
-        if (trigger=="Fall"&& Mathf.Abs(rigid2D.velocity.y) < 1e-5f)
-            TriggerChange(Trigger.Idle);
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            TriggerChange(Trigger.Run);
+        if (trigger == "Fall" && Mathf.Abs(rigid2D.velocity.y) < 1e-4f)
+        { 
+            TriggerChange(Trigger.Idle); 
         }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             key = -1;
+            if (trigger == "Idle") TriggerChange(Trigger.Run);
+            if (speed < maxSpeed)
+                rigid2D.AddForce(transform.right * walkForce * key);
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             TriggerChange(Trigger.Idle);
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            TriggerChange(Trigger.Run);
-        }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             key = 1;
+            if (trigger == "Idle") TriggerChange(Trigger.Run);
+            if (speed < maxSpeed)
+                rigid2D.AddForce(transform.right * walkForce * key);
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {    TriggerChange(Trigger.Idle);
         }
-        if (speed < maxSpeed)
-            rigid2D.AddForce(transform.right * walkForce*key);
+        
 
         if (key!=0)
             transform.localScale = new Vector3(key,1,1);
-        //Debug.Log($"{trigger}{rigid2D.velocity.y}");
         
     }
 }
