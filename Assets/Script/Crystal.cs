138486a0b51da8292bca9a743f704fc2b783c7e2
s0993056 <s0993056@alumni.ncyu.edu.tw>
2022-06-28 15:27:14 +0800

    6/28

--- Assets/Script/Crystal.cs
+++ Assets/Script/Crystal.cs
@@ -12,7 +12,7 @@ public class Crystal : MonoBehaviour
     void Update()
     {
         if (GameController.clickNumber < 2) transform.position = new Vector3(Player.transform.position.x - 6f, Player.transform.position.y + 2, 0);//一開始鏡頭外
-        if (GameController.clickNumber >= 2)//6小精靈出現慢移
+        if (GameController.clickNumber >= 2)//小精靈出現慢移
             transform.position = new Vector3(Mathf.Lerp(transform.position.x, Player.transform.position.x - 1.3f, 1.2f * Time.deltaTime), Player.transform.position.y + 2, 0);
         if((Step)GameController.step >= Step.Run)
             transform.position = new Vector3( Player.transform.position.x-1.3f, Player.transform.position.y+1.55f,0);
